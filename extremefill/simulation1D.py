a7aeb184e515b0fc096513f40d87146ea110124c
Daniel Wheeler <daniel.wheeler2@gmail.com>
2012-10-19 15:32:15 -0400

    Simulation1D runs with all it's equations.
    This version should run the whole 1D problem with a stationary
    interface.

--- extremefill/simulation1D.py
+++ extremefill/simulation1D.py
@@ -4,9 +4,8 @@ __docformat__ = 'restructuredtext'
 
 import fipy as fp
 from fipy import numerix as nx
-import numpy as np
 
-class Simulation(object):
+class Simulation1D(object):
     r"""
 
     This class solves the 1D extreme fill problem modeled with the
@@ -99,13 +98,16 @@ class Simulation(object):
     feature) with the simple 1D ODE for solving the electrical equation
     with no suppressor and no cupric depeletion.
 
+    >>> import numpy as np
+
+    
     >>> i0 = 40.
     >>> alpha = 0.4
     >>> F = 9.6485e4 ## C / mol = J / V / mol
     >>> R = 8.314 ## J / K / mol
     >>> T = 298. ## K
     >>> appliedPotential = -0.275
-    >>> simulation = Simulation()
+    >>> simulation = Simulation1D()
     >>> simulation.run(delta=100e-6,
     ...                deltaRef=200e-6,
     ...                i0=i0,
@@ -121,7 +123,8 @@ class Simulation(object):
     ...                dtMax=.5e-7,
     ...                sweeps=5)
 
-    >>> timesScipy, potentialsScipy = SimulationODE().run(deltaRef=200e-6)
+    >>> from simulation1DODE import Simulation1DODE
+    >>> timesScipy, potentialsScipy = Simulation1DODE().run(deltaRef=200e-6)
     >>> print np.allclose(simulation.parameters['potentials'], potentialsScipy, atol=1e-4)
     True
 
@@ -148,7 +151,7 @@ class Simulation(object):
     >>> charge = 2
     >>> cinf = 1000.
 
-    >>> simulation = Simulation()
+    >>> simulation = Simulation1D()
     >>> simulation.run(i0=i0,
     ...                alpha=alpha,
     ...                i1=0.0,
@@ -170,11 +173,7 @@ class Simulation(object):
     ...     V = simulation.parameters['potentials'][-1] 
     ...     return i0 * (np.exp(-alpha * Fbar * V) - np.exp((2 - alpha) * Fbar * V))
 
-    >>> cupric = simulation.parameters['cupric']
-    >>> if cupric.shape is ():
-    ...     cupric0 = cupric
-    ... else:
-    ...     cupric0 = cupric[0]
+    >>> cupric0 = simulation.parameters['cupric0']
 
     >>> print np.allclose(1 / (1 + iF0() * delta / D / charge / F / cinf), cupric0 / cinf, rtol=1e-3)
     True
@@ -255,14 +254,14 @@ class Simulation(object):
         self.parameters['trenchWidth'] = 2 * 0.093 / perimeterRatio
         self.parameters['fieldWidth'] = 2 / perimeterRatio
         
-        epsilon = 1e-30 
-
         distanceBelowTrench = delta * 0.1
         L = delta + distanceBelowTrench
         N = 1000
         dx = L / N 
         mesh = fp.Grid1D(nx=N, dx=dx) - [[distanceBelowTrench]]
 
+        dt = fp.Variable(dt)
+        
         distance = fp.DistanceVariable(mesh=mesh)
         distance[:] = 1.
         distance.setValue(-1., where=mesh.x < 0)
@@ -270,18 +269,15 @@ class Simulation(object):
         potential = fp.CellVariable(mesh=mesh, hasOld=True, name=r'$\psi$')
         potential[:] = -appliedPotential
 
-        # cupric = fp.CellVariable(mesh=mesh, hasOld=True, name=r'$c_{cu}$')
-        # cupric[:] = bulkCupric
-        # cupric.constrain(bulkCupric, mesh.facesRight)
-        cupric = bulkCupric
+        cupric = fp.CellVariable(mesh=mesh, hasOld=True, name=r'$c_{cu}$')
+        cupric[:] = bulkCupric
+        cupric.constrain(bulkCupric, mesh.facesRight)
         
-        # suppressor = fp.CellVariable(mesh=mesh, hasOld=True, name=r'$c_{\theta}$')
-        # suppressor[:] = bulkSuppressor
-        # suppressor.constrain(bulkSuppressor, mesh.facesRight)
-        suppressor = bulkSuppressor
+        suppressor = fp.CellVariable(mesh=mesh, hasOld=True, name=r'$c_{\theta}$')
+        suppressor[:] = bulkSuppressor
+        suppressor.constrain(bulkSuppressor, mesh.facesRight)
         
-        # theta = fp.CellVariable(mesh=mesh, hasOld=True, name=r'$\theta$')
-        theta = 0.
+        theta = fp.SurfactantVariable(distanceVar=distance, hasOld=True, name=r'$\theta$', value=0.)
 
         I0 = (i0 + i1 * theta)
         baseCurrent = I0 * (nx.exp(alpha * Fbar * potential) \
@@ -291,35 +287,31 @@ class Simulation(object):
         currentDerivative = cbar * I0 * (alpha * Fbar *  nx.exp(alpha * Fbar * potential) \
                                              + (2 - alpha) * Fbar * nx.exp(-(2 - alpha) * Fbar * potential))
 
-        # def dirac(x):
-        #     value = numerix.zeros(mesh.numberOfCells, 'd')
-        #     ID = numerix.argmin(abs(mesh.x - x))
-        #     if mesh.x[ID] < x:
-        #         ID = ID + 1
-        #     value[ID] = 1. / dx
-        #     return value
-
-        # THETA = (mesh.x < 0) * perimeterRatio + dirac(0) * (1 - areaRatio) + dirac(-featureDepth) * areaRatio
-        # AREA = (mesh.x < 0) * (areaRatio - 1) + 1 
         upper = fp.CellVariable(mesh=mesh)
         upper[-1] = kappa / dx / (deltaRef - delta)
 
         surface = distance.cellInterfaceAreas / mesh.cellVolumes
-
+        diffCoeff = (distance >= 0).harmonicFaceValue
         
-        potentialEq = fp.TransientTerm(capacitance * surface + (distance < 0)) == fp.DiffusionTerm(kappa) \
+        potentialEq = fp.TransientTerm(capacitance * surface + (distance < 0)) == \
+          fp.DiffusionTerm(kappa * diffCoeff) \
           - surface * (current - potential * currentDerivative) \
-            - fp.ImplicitSourceTerm(surface * currentDerivative) \
-            - upper * appliedPotential - fp.ImplicitSourceTerm(upper) 
+          - fp.ImplicitSourceTerm(surface * currentDerivative) \
+          - upper * appliedPotential - fp.ImplicitSourceTerm(upper) 
             
-        # cupricEq = fp.TransientTerm(AREA) == fp.DiffusionTerm(diffusionCupric * AREA) \
-        #     - fp.ImplicitSourceTerm(baseCurrent * THETA / (bulkCupric * charge * faradaysConstant))
+        cupricEq = fp.TransientTerm() == fp.DiffusionTerm(diffusionCupric * diffCoeff) \
+          - fp.ImplicitSourceTerm(baseCurrent * surface / (bulkCupric * charge * faradaysConstant))
 
-        # suppressorEq = fp.TransientTerm(AREA) == fp.DiffusionTerm(diffusionSuppressor * AREA) \
-        #     - fp.ImplicitSourceTerm(gamma * kPlus * (1 - theta) * THETA)
+        suppressorEq = fp.TransientTerm() == fp.DiffusionTerm(diffusionSuppressor * diffCoeff) \
+          - fp.ImplicitSourceTerm(gamma * kPlus * (1 - theta.interfaceVar) * surface)
 
-        # thetaEq =  fp.TransientTerm(THETA + epsilon) == kPlus * suppressor * THETA \
-        #     - fp.ImplicitSourceTerm(THETA * (kPlus * suppressor + kMinus * current * (omega / charge / faradaysConstant)))
+        depositionRate = current * omega / charge / faradaysConstant
+        adsorptionCoeff = dt * suppressor * kPlus
+        thetaEq = fp.TransientTerm() == fp.ExplicitUpwindConvectionTerm(fp.SurfactantConvectionVariable(distance)) \
+          + adsorptionCoeff * surface \
+          - fp.ImplicitSourceTerm(adsorptionCoeff * distance._cellInterfaceFlag) \
+          - fp.ImplicitSourceTerm(kMinus * depositionRate * dt)
+        theta.constrain(0, mesh.exteriorFaces)
 
         t = 0.
 
@@ -339,16 +331,16 @@ class Simulation(object):
                 viewer.plot()
 
             potential.updateOld()
-            # cupric.updateOld()
-            # suppressor.updateOld()
-            # theta.updateOld()
+            cupric.updateOld()
+            suppressor.updateOld()
+            theta.updateOld()
 
             for sweep in range(sweeps):
                 potentialRes = potentialEq.sweep(potential, dt=dt)
-                # cupricRes = cupricEq.sweep(cupric, dt=dt)
-                # suppressorRes = suppressorEq.sweep(suppressor, dt=dt)
-                # thetaRes = thetaEq.sweep(theta, dt=dt)
-                cupricRes = suppressorRes = thetaRes = 0.0
+                cupricRes = cupricEq.sweep(cupric, dt=dt)
+                suppressorRes = suppressorEq.sweep(suppressor, dt=dt)
+                thetaRes = thetaEq.sweep(theta, dt=dt)
+                thetaRes = 0.0
                 res = nx.array((potentialRes, cupricRes, suppressorRes, thetaRes))
                 if sweep == 0:
                     res0 = res
@@ -373,12 +365,13 @@ class Simulation(object):
                 print 'dt',dt
                 print 'step',step
 
-            t += dt
+            t += float(dt)
 
-            dt = dt * 1e+10
-            dt = min((dt, dtMax))
-            dt = max((dt, dtMin))
-            potentials.append(-float(potential[0]))
+            dt.setValue(float(dt) * 1e+10)
+            dt.setValue(min((float(dt), dtMax)))
+            dt.setValue(max((float(dt), dtMin)))
+            
+            potentials.append(-float(potential([[dx / 2]])))
         if view:
             viewer.plot()
 
@@ -387,57 +380,8 @@ class Simulation(object):
         self.parameters['suppressor'] = nx.array(suppressor)
         self.parameters['theta'] = nx.array(theta)
         self.parameters['potentials'] = nx.array(potentials)
+        self.parameters['cupric0'] = nx.array(cupric([[dx / 2]]))
 
-class SimulationODE(object):
-    def run(self,
-            delta = 100e-6, ## m
-            deltaRef = 50e-6, ## m
-            faradaysConstant = 9.6485e4, ## C / mol = J / V / mol
-            gasConstant = 8.314, ## J / K / mol
-            temperature = 298., ## K
-            alpha = 0.4,
-            appliedVoltage = -0.275,  ## V
-            i0 = 40., ## A / m**2 
-            capacitance = 0.3, ## F / m**2 = A s / V / m**2  
-            kappa = 15.26): ## S / m = A / V / m):
-
-        Fbar = faradaysConstant / gasConstant / temperature ## 1 / V
-    
-        def iF(potential):
-            return i0 * (np.exp(alpha * Fbar * potential) - np.exp(-(2 - alpha) * Fbar * potential))
-
-        def iFderivative(potential):
-            return i0 * (alpha * Fbar * np.exp(alpha * Fbar * potential) \
-                   + (2 - alpha) * Fbar * np.exp(-(2 - alpha) * Fbar * potential))
-
-        def RHS(t, y):
-            potential = y[0]
-            return np.array((-iF(potential) / capacitance  - kappa * (potential + appliedVoltage) / deltaRef / capacitance,))
-
-        def jacobian(t, y):
-            potential = y[0]
-            return np.array((-iFderivative(potential) / capacitance  - kappa / deltaRef / capacitance,))
-
-        from scipy.integrate import ode
-        integrator = ode(RHS, jacobian).set_integrator('vode', method='bdf', with_jacobian=True)
-        initialValue = -appliedVoltage # + (delta - deltaRef) / deltaRef * appliedVoltage
-        s = integrator.set_initial_value((initialValue,), 0.)
-
-        totalSteps = 200
-        dt = .5e-7
-        times = np.zeros(totalSteps)
-        potentialSciPy = np.zeros(totalSteps)
-        step = 0
-
-        while integrator.successful() and step < totalSteps:
-            null = integrator.integrate(integrator.t + dt)
-            times[step] = integrator.t
-            potential =  integrator.y[0]
-            potentialSciPy[step] = -potential
-            step += 1
-
-        return np.array(times), np.array(potentialSciPy) 
- 
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
