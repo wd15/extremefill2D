f0944beceeea4766a77084d68ac232777a9f0f79
Daniel Wheeler <daniel.wheeler2@gmail.com>
2012-11-28 11:27:42 -0500

    The datafile is now a temporary created on the local machine.

--- CFL/tmp0.4.py
+++ CFL/tmp0.4.py
@@ -1,7 +1,15 @@
 from extremefill.simulation2D import Simulation2D
+import tempfile
+import os
 
 simulation = Simulation2D()
+(f, datafile) = tempfile.mkstemp(suffix='.h5')
+os.close(f)
+
 simulation.run(view=False, totalSteps=1e+10, sweeps=30, dt=0.01, tol=1e-1, Nx=300, CFL=0.4, PRINT=True, areaRatio=2 * 0.093, dtMax=100., dataFile='cfl0.4.h5', totalTime=5000., Nnarrow=100000000000)
+
+
+os.remove(datafile)
 
 
 
