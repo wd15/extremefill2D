4c257b1e61b0fe0bfc34b74560990f0716f4c167
Daniel Wheeler <daniel.wheeler2@gmail.com>
2012-12-07 16:46:59 -0500

    Adding new script

--- data/script.py
+++ data/script.py
@@ -3,7 +3,7 @@ from extremefill.simulation2D import Simulation2D
 def run(CFL=0.2, Nx=300):
     simulation = Simulation2D()
     simulation.run(view=False,
-                   totalSteps=12,
+                   totalSteps=1000000000000,
                    sweeps=30,
                    dt=0.01,
                    tol=1e-1,
@@ -20,10 +20,8 @@ def run(CFL=0.2, Nx=300):
 
 if __name__ == '__main__':
     from gitqsub import qsubmit
-    # for CFL in (0.025, 0.05, 0.1, 0.2, 0.4, 0.8):
-    #     qsubmit(callBack=run, newbranch='CFL' + str(CFL), CFL=CFL)
+    for CFL in (0.025, 0.05, 0.1, 0.2, 0.4, 0.8):
+        qsubmit(callBack=run, newbranch='CFL' + str(CFL), CFL=CFL)
 
-    # for Nx in (150, 300, 600, 1200):
-    #     qsubmit(callBack=run, newbranch='Nx' + str(Nx), Nx=Nx)
-    CFL = 0.4
-    qsubmit(callBack=run, newbranch='CFL' + str(CFL), CFL=CFL)
+    for Nx in (150, 300, 600, 1200):
+        qsubmit(callBack=run, newbranch='Nx' + str(Nx), Nx=Nx)
