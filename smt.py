31c39e8e69200183dfd8cfbef9f73de28b8ab2f9
Daniel Wheeler <daniel.wheeler2@gmail.com>
2013-02-01 18:16:02 -0500

    Using Simulation.datastore instead of Record.datastore

--- smt.py
+++ smt.py
@@ -86,6 +86,7 @@ class BatchSimulation(object):
             simulations += [simulation]
 
         while not np.all([s.finished for s in simulations]):
+            print simulations[0].process.poll()
             print not np.all([s.finished for s in simulations])
             time.sleep(poll_time)
 
