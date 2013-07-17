6b86f217637390bcdd8d4c563a934905fe96a42c
Daniel Wheeler <daniel.wheeler2@gmail.com>
2013-07-17 18:01:21 -0400

    Sumatra can only add one tag at the command line currently

--- batch.py
+++ batch.py
@@ -1,11 +1,11 @@
 import os
 
-def batch_launch(reason='', tags=[], serialnumber=1, **kwargs):
+def batch_launch(reason='', tags=[], **kwargs):
     cmd = 'qsub -cwd -o {0} -e {0} launcher'.format('.qsub')
     for k, v in kwargs.iteritems():
         cmd += ' {0}={1}'.format(k, v)
     cmd += ' --reason="{0}"'.format(reason)
-    for t in ['qsub', 'serialnumber{0}'.format(serialnumber)] + tags:
+    for t in tags:
         cmd += ' --tag={0}'.format(t)
     print cmd
     os.system(cmd)
