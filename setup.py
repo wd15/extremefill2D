986f913c7044b73395cd415a99adee0ffea831b2
Daniel Wheeler <daniel.wheeler2@gmail.com>
2013-11-14 17:29:44 -0500

    Adding setup.py for extremefill2D package.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+setup(name='extremefill2D',
+      version='0.1',
+      description='Python package to generate figures and run simulations for the Extremefill 2D paper',
+      author='Daniel Wheeler',
+      author_email='daniel.wheeler2@gmail.com',
+      url='http://wd15.github.com/extremefill2D',
+      packages=find_packages()
+      )
