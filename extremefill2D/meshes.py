8e423c3168c65fd2de6ccd5a0c14a5d9cf29785a
Daniel Wheeler <daniel.wheeler2@gmail.com>
2014-04-15 17:10:04 -0400

    Fix `ExtremeFill2DMesh` docstrings
    Address #16
    
    Expand docstrings in mesh class

--- extremefill2D/meshes.py
+++ extremefill2D/meshes.py
@@ -5,7 +5,27 @@ from fipy.meshes.cylindricalNonUniformGrid2D import CylindricalNonUniformGrid2D
 
 
 class ExtremeFill2DMesh(CylindricalNonUniformGrid2D):
+    """Mesh class for via or annular meshes
+
+    A cylindrical grid which is fine in the region close to the
+    feature and coarsens away for the the feature
+
+    The grid is coarsened away from the feature using a geometric
+    ratio.
+    
+    Attributes:
+        `nominal_dx`: The ideal value for `dx`. Not the actual value of `dx`
+        
+    """
     def __init__(self, params):
+        """Init for `ExtremeFill2DMesh`.
+
+        Args: 
+            params: a class with `Nx`, `rinner`, `router`, `rboundary`,
+                `spacing_ratio`, `featureDepth` and `delta` attributes.
+
+        """
+
         dy = params.featureDepth / params.Nx
         dx = dy
         distanceBelowTrench = 10 * dx
@@ -28,10 +48,22 @@ class ExtremeFill2DMesh(CylindricalNonUniformGrid2D):
         self.nominal_dx = dx
 
     def get_nonuniform_dx(self, dx, x0, x1, x2, padding, spacing_ratio=1.1):
-        """
+        """Calculates mesh spacing for a triple layered domain.
 
-        Calculate the geometric grid spacing for a grid with an fine grid
-        between `r0` and `r1` and a coarse grid elsewhere.
+        Calculates the spacing for a triple layer domain where the
+        central region is fine and the outer regions are coarsened
+        geometrically. The domain starts at 0 and extends out
+
+        Args:
+            `dx`: the ideal spacing in the fine region
+            `x0`: the inner position of the fine region
+            `x1`: the outer position of the fine region
+            `x2`: the outer extent of the domain
+            `padding`: the width of padding where the spacing in fine around the nominal fine region
+            `spacing_ratio`: the geometric ration to use when coarsening the mesh
+            
+        Returns:
+            an array of the spacing
 
         """
         assert (x2 - x1) > padding
@@ -48,11 +80,16 @@ class ExtremeFill2DMesh(CylindricalNonUniformGrid2D):
         return np.concatenate((dx0, dx1, dx2))
 
     def geometric_spacing(self, initial_spacing, domain_size, spacing_ratio=1.1):
-        """
+        """Calculate geometric spacing.
 
-        Caculate grid spacing in one dimension using the inital grid cell
-        size given by `initial_spacing`. The grid cells are scaled up by
-        `spacing_ratio` and `spacing ratio` must be greater than 1.
+        Calculate grid spacing in one dimension starting with an
+        initial spacing and coarsening from right to left
+        geometrically.
+
+        Args:
+            `initial_spacing`: the left hand fine grid spacing
+            `domain_size`: the size of the domain
+            `spacing_ration`: the geometric ratio to use for coarsening the mesh
 
         """
         assert spacing_ratio > 1.
