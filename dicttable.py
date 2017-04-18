95a25a23e7aec4c2d8a37594f4f1be628c5a2757
Daniel Wheeler <daniel.wheeler2@gmail.com>
2017-04-18 16:38:50 -0400

    Update to new version of tables

--- dicttable.py
+++ dicttable.py
@@ -15,14 +15,14 @@ class DictTable:
         self.h5filename = h5filename
 
     def haskey(self, index):
-        h5file = tables.openFile(self.h5filename, mode='a')
+        h5file = tables.open_file(self.h5filename, mode='a')
         groupName = self.IDprefix + str(index)
         _haskey =  hasattr(h5file.root, groupName)
         h5file.close()
         return _haskey
 
     def __setitem__(self, index, values):
-        h5file = tables.openFile(self.h5filename, mode='a')
+        h5file = tables.open_file(self.h5filename, mode='a')
         h5file.root._v_attrs.latestIndex = index
 
         groupName = self.IDprefix + str(index)
@@ -30,17 +30,17 @@ class DictTable:
         if hasattr(h5file.root, groupName):
             group = h5file.root._f_getChild(groupName)
             group._f_remove(recursive=True)
-            
-        group = h5file.createGroup(h5file.root, groupName)
+
+        group = h5file.create_group(h5file.root, groupName)
 
         for k in values.keys():
-            h5file.createArray(group, k, values[k])
+            h5file.create_array(group, k, values[k])
 
         h5file.close()
 
     def openread(self):
         if not hasattr(self, 'h5fileread'):
-            self.h5fileread = tables.openFile(self.h5filename, mode='r')
+            self.h5fileread = tables.open_file(self.h5filename, mode='r')
         return self.h5fileread
 
     def __getitem__(self, index):
@@ -48,12 +48,12 @@ class DictTable:
 
         if type(index) is int:
             index = [index]
-        
+
         s = '/' + self.IDprefix + str(index[0])
 
         if len(index) == 1:
             d = {}
-            for array in h5file.listNodes(s, classname='Array'):
+            for array in h5file.list_nodes(s, classname='Array'):
                 d[array.name] = array.read()
         else:
             for t in index[1:]:
@@ -72,6 +72,3 @@ class DictTable:
         if hasattr(self, 'h5fileread'):
             self.h5fileread.close()
             del self.h5fileread
-
-        
-    
